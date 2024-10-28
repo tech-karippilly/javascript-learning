5a3f6605def133fa93c556643bf3228cefd11b0a
“tech-karippilly” <vishnukarippilly@gmail.com>
2024-10-28 13:50:47 +0530

    two added

--- script.js
+++ script.js
@@ -1,1 +1,58 @@
 
+const markForm = document.getElementById('passOrFailForm');
+
+markForm.addEventListener('submit',checkResult)
+
+function checkResult(event){
+    event.preventDefault();
+
+    const obtainedMark = document.getElementById('student-mark');
+
+    const markDisplay = document.getElementById('mark-result');
+    const markValue = parseFloat(obtainedMark.value);
+    if(isNaN(markValue)){
+       markDisplay.style.color='ornage';
+        markDisplay.textContent ='Error::Please enter valid number'
+    }else  if (parseFloat(obtainedMark.value) >= 50){
+    
+        markDisplay.style.color='green';
+        markDisplay.textContent ='Result: Pass'
+    }else{
+        markDisplay.style.color='red';
+        markDisplay.textContent ='Result: FAILED'
+    }
+}
+
+const gradeFrom = document.getElementById('gradeForm');
+
+gradeFrom.addEventListener('submit',checkGrande)
+
+function checkGrande(event){
+    event.preventDefault();
+
+    const  percentage = document.getElementById('student-percentage')
+    const markDisplay = document.getElementById('grade-Result');
+    const markValue = parseFloat(percentage.value);
+    if (isNaN(markValue) || markValue <=0){
+              markDisplay.style.color='ornage';
+        markDisplay.textContent ='Error::Please enter valid number'
+    }else if (markValue >=90 ){
+        markDisplay.style.color='green';
+        markDisplay.textContent ='Grade : A' 
+    }else if (markValue >=80 ){
+        markDisplay.style.color='green';
+        markDisplay.textContent ='Grade : B' 
+    }else if (markValue >=70 ){
+        markDisplay.style.color='Yellow';
+        markDisplay.textContent ='Grade : C' 
+    }else if (markValue >=60 ){
+        markDisplay.style.color='Orange';
+        markDisplay.textContent ='Grade : D' 
+    }else if (markValue >=50 ){
+        markDisplay.style.color='Orange';
+        markDisplay.textContent ='Grade : E' 
+    }else if (markValue < 50 ){
+        markDisplay.style.color='Red';
+        markDisplay.textContent ='Grade : D' 
+    }
+}
