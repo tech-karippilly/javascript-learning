1be31969946ab988a84ab83d70a464b980b61163
“tech-karippilly” <vishnukarippilly@gmail.com>
2024-10-29 19:45:32 +0530

    working on it

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 
+const stringForm = document.getElementById('string-from');
+
+stringForm.addEventListener('submit',checkPallindrome)
+
+
+function checkPallindrome(event){
+
+    event.preventDefault()
+
+    const string = document.getElementById('string').value
+
+    const smallCaseString = string.toLowerCase();
+
+    const reversedString= smallCaseString.split('').reverse().join('');
+
+    const resultString = document.getElementById('result');
+    if (smallCaseString === reversedString){
+        resultString.style.color='green'
+        resultString.innerText='String is Pallindrome '
+    }else{
+        resultString.style.color='red'
+         resultString.innerText='String is not Pallindrome '
+    }
+
+
+
+} 
