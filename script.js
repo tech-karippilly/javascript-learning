b52c875c1e8708638c34657f2b7a07ec18f2ea2c
“tech-karippilly” <vishnukarippilly@gmail.com>
2024-10-29 09:40:45 +0530

    first assginment

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
+//ARRAY DECLARATION
+const arryaOne =[]
+const arrayTwo =[]
 
+const arrayOneBeforeDisplay = document.getElementById('arrayOne-beforeSwap')
+const arrayTwoBeforeDisplay =document.getElementById('arrayTwo-beforeSwap')
+
+// ARRAY DISPLAY ELEMENTS DOM
+const arrayOneDisplay = document.getElementById('arrayOne')
+const arrayTwoDisplay =document.getElementById('arrayTwo')
+
+//size of array
+const size =5
+
+//ARRAY INSERTION
+for (let i = 0; i < size; i++) {
+    const value = prompt(`Enter value ${i + 1}:`);
+    if (value !== null) {
+        arryaOne.push(value); 
+    }
+}
+//ARRAY INSERTION
+for (let i = 0; i < size; i++) {
+    const value = prompt(`Enter value ${i + 1}:`);
+    if (value !== null) {
+        arrayTwo.push(value); 
+    }
+}
+
+arrayOneBeforeDisplay.innerHTML ="Array One:" +arryaOne.toString()
+arrayTwoBeforeDisplay.innerHTML = "Array Two:" +arrayTwo.toString()
+
+
+// ARRAY SWAPPING
+let temp = 0
+
+for (let  i=0;i<arryaOne.length;i++){
+    temp = arryaOne[i]
+    arryaOne[i] = arrayTwo[i]
+    arrayTwo[i] = temp
+}
+
+// ARRAY DISPLAY
+arrayOneDisplay.innerHTML ="Array One:" +arryaOne.toString()
+arrayTwoDisplay.innerHTML = "Array Two:" +arrayTwo.toString()
