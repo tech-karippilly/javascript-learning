de604d945d9adfcd404cf789dc15ce0fa456fc44
“tech-karippilly” <vishnukarippilly@gmail.com>
2024-10-31 23:07:39 +0530

    check height

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
+const displayMessage = document.getElementById('display')
+const heightForm = document.getElementById('height-form')
 
+heightForm.addEventListener('submit',myFunction)
+
+function myFunction(event){
+    event.preventDefault()
+
+    const height = document.getElementById('height').value
+
+   
+    try{
+        checkHeight(height)
+    }catch(error){
+        displayMessage.style.color='red'
+        displayMessage.innerText=error.message
+        console.log(error.message);
+    }
+}
+
+function checkHeight(height){
+    if (isNaN(height)) {
+        throw new Error("notANumberError");
+    }
+
+    height = Number(height);
+
+    if (height > 75) {
+        throw new Error("hugeHeightError");
+    }
+
+    if (height < 1) {
+        throw new Error("tinyHeightError");
+    }
+
+    displayMessage.style.color='green'
+    displayMessage.innerText=height
+}
